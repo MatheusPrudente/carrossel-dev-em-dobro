aedc1bf6b82a057df07caefcea220b5bdb35dc21
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-03-12 19:57:55 -0300

    Adicionar um intervalo de rotação no carrousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,77 @@
 const carousel = document.querySelector(".carousel");
 const carouselButtons = document.querySelector(".carousel-buttons");
+const information = document.querySelector(".information");
+const scrollTime = 4000;
+
+const scroll = setInterval(() => {
+  const buttons = document.querySelectorAll(".button");
+
+  for (let index = 0; index < buttons.length; index++) {
+    let element = buttons[index];
+    if (element.classList.contains("selected")) {
+      let nextElement = buttons[0];
+      let nextIndex = 0;
+
+      if (buttons[index + 1]) {
+        nextIndex = index + 1;
+        nextElement = buttons[nextIndex];
+      }
+
+      rotateCarousel(nextElement, nextIndex);
+      break;
+    }
+  }
+}, scrollTime);
 
 fetch("../src/data/data.json")
   .then((response) => {
     return response.json()
   }).then((data) => {
-    data.forEach((d, index ) => {
-      let image = document.createElement("img");
-      image.src = d.source;
-      image.alt = d.alternateText;
-      image.classList.add("carousel-image");
-
-      let button = document.createElement("button");
-      button.classList.add("button");
-
-      if(index == 0) {
-        image.classList.add("actived")
-        button.classList.add("selected")
-      }
-
-      button.addEventListener("click", () => {
-        disableSelectedButton();
-        button.classList.add("selected");
-        hideActiveImage();
-        showBackgroundImage(index);
-      });
-
-      carousel.appendChild(image);
-      carouselButtons.appendChild(button);
-    })
+    show(data);
   });
+
+function show(data) {
+  let logo = document.createElement("img");
+  logo.src = data.logo.source;
+  logo.alt = data.logo.alternateText;
+  logo.classList.add("logo");
+
+  let description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = data.description;
+
+  information.appendChild(logo);
+  information.appendChild(description);
+
+  data.gallery.forEach((image, index) => {
+    let imageElement = document.createElement("img");
+    imageElement.src = image.source;
+    imageElement.alt = image.alternateText;
+    imageElement.classList.add("carousel-image");
+
+    let buttonElement = document.createElement("button");
+    buttonElement.classList.add("button");
+
+    if (index == 0) {
+      imageElement.classList.add("actived")
+      buttonElement.classList.add("selected")
+    }
+
+    buttonElement.addEventListener("click", () => {
+      rotateCarousel(buttonElement, index);
+    });
+
+    carousel.appendChild(imageElement);
+    carouselButtons.appendChild(buttonElement);
+  })
+}
+
+function rotateCarousel(element, index) {
+  disableSelectedButton();
+  element.classList.add("selected");
+  hideActiveImage();
+  showBackgroundImage(index);
+}
 
 function disableSelectedButton() {
   const button = document.querySelector(".selected");
